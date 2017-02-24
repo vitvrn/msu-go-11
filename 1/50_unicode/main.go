951e0cb02e80ea8db2a86f4d479eafe117b5d9a8
Vasily Romanov <v.romanov@corp.mail.ru>
2017-02-24 20:59:10 +0300

    0-60

---
+++ 1/50_unicode/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	var symbol rune = 'a'
+	var autoSymbol = 'a'
+	var unicodeSymbol rune = '⌘'
+	var uncideSymboldByNumber rune = '\u2318'
+
+	println(symbol, autoSymbol, unicodeSymbol, uncideSymboldByNumber)
+}
