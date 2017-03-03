8945453ab47a1ef5140e30f2f5864ee16d926022
iozherelyev <iozherelyev@gmail.com>
2017-03-04 01:13:29 +0300

    added description for type switch case

--- 2/99_homework/funcs/2/main_test.go
+++ 2/99_homework/funcs/2/main_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestType(t *testing.T) {
-	for i, tt := range []interface{}{1, uint(2), int8(4), float64(7), "", 'w'} {
+	for i, tt := range []interface{}{1, uint(2), int8(4), float64(7), "", 'w', []int{}, map[string]bool{}} {
 		if showMeTheType(tt) != fmt.Sprintf("%T", tt) {
 			t.Errorf("Case[%d]: failed res %s, expected %T", i, showMeTheType(tt), tt)
 		}
