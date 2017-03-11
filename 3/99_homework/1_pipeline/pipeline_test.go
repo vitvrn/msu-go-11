7db6e83a981ca7d8c94a1c505d5c9a83298d8c5e
Vasily <romanov.vasily@mail.ru>
2017-03-11 19:58:02 +0300

    formatting

--- 3/99_homework/1_pipeline/pipeline_test.go
+++ 3/99_homework/1_pipeline/pipeline_test.go
@@ -8,11 +8,12 @@ import (
 
 func TestMain(t *testing.T) {
 	res := []interface{}{}
-	case1 := []job{job(func(in, out chan interface{}) {
-		for i := 0; i < 10; i++ {
-			out <- i
-		}
-	}),
+	case1 := []job{
+		job(func(in, out chan interface{}) {
+			for i := 0; i < 10; i++ {
+				out <- i
+			}
+		}),
 		job(func(in, out chan interface{}) {
 			for i := range in {
 				if s, ok := i.(int); ok && (s%3 == 0) {
@@ -27,7 +28,8 @@ func TestMain(t *testing.T) {
 			for val := range in {
 				res = append(res, val)
 			}
-		})}
+		}),
+	}
 
 	expected := []interface{}{0, 30, 60, 90}
 
@@ -41,23 +43,26 @@ func TestMain(t *testing.T) {
 
 	res2 := ""
 
-	case2 := []job{job(func(in, out chan interface{}) {
-		for _, word := range []string{"Hello", "World"} {
-			out <- word
-		}
-	}), job(func(in, out chan interface{}) {
-		for word := range in {
-			fmt.Println("Got", word)
-			if w, ok := word.(string); ok {
-				fmt.Println("And it is a string")
-				if len(res2) > 0 {
-					res2 = res2 + " " + w
-				} else {
-					res2 = w
+	case2 := []job{
+		job(func(in, out chan interface{}) {
+			for _, word := range []string{"Hello", "World"} {
+				out <- word
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for word := range in {
+				fmt.Println("Got", word)
+				if w, ok := word.(string); ok {
+					fmt.Println("And it is a string")
+					if len(res2) > 0 {
+						res2 = res2 + " " + w
+					} else {
+						res2 = w
+					}
 				}
 			}
-		}
-	})}
+		}),
+	}
 
 	Pipe(case2...)
 	expected2 := "Hello World"
