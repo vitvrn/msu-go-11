d64a766565ae6d7d22c4f5a591ad17a698885d69
Vasily Romanov <v.romanov@corp.mail.ru>
2017-03-25 01:31:04 +0300

    5th

--- 5/01_cookie/main.go
+++ 5/01_cookie/main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+var commentFormTmpl = `
+<html>
+	<body>
+	<form action="/post_comment" method="post">
+		Login: <input type="text" name="login"><br>
+		<textarea name="comment"></textarea><br>
+		<input type="submit">
+	</form>
+	</body>
+</html>
+`
 
 func main() {
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// w.Write([]byte(body))
+	})
+
+	http.HandleFunc("/get_cookie", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.ListenAndServe(":8081", nil)
+}
+
+//PanicOnErr panics on error
+func PanicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
