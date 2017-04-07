6d97dffb1169b4c427424c842514a68ac1109edf
Vasily <romanov.vasily@mail.ru>
2017-04-07 23:10:40 +0300

    mock

--- 6/06_mock/users/users.go
+++ 6/06_mock/users/users.go
@@ -1,17 +1,22 @@
 package users
 
+// если мы хотим тестить юзера через моки, то мы должны положить тесты в отдлельный файл
+// потому что моки импортируют юзера и если мы будем импортировать моке, то получится циклическая зависимость
+
 type User struct {
-	ID int
+	ID   int
 	Name string
-	Age int
 }
 
-type Users interface {
-	Get(id int) User
-	Put(id int, name string, age int)
+func (u *User) GetName() string {
+	return u.Name
 }
 
-func DoSomethingWithUsers(users *Users) {
-	(*users).Put(500, "name", 7)
+func (u *User) SetName(newName string) {
+	u.Name = newName
 }
 
+type UserInterface interface {
+	GetName() string
+	SetName(string)
+}
