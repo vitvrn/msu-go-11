fda757b22d355932f91c51b61cbc8a6899f528e8
Vasily Romanov <bloodsin.romanov@gmail.com>
2017-04-30 08:37:54 +0000

    Update mysql_db.sql

--- 7/99_homework/myorm/user/mysql_db.sql
+++ 7/99_homework/myorm/user/mysql_db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `login` varchar(300) NOT NULL,
+  `username` varchar(300) NOT NULL,
   `info` text NULL,
   `balance` int NOT NULL
   `status` int NOT NULL
 );
 
-INSERT INTO `users` (`id`, `login`, `info`, `balance`, `status`) VALUES (1, 'Vasily Romanov', 'company: Mail.ru Group', 100500, 1);
+INSERT INTO `users` (`id`, `username`, `info`, `balance`, `status`) VALUES (1, 'Vasily Romanov', 'company: Mail.ru Group', 100500, 1);
